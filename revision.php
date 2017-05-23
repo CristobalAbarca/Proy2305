88f471edd002fcbb2c3f9a02b6cd0e93539d68db
cetecom <cetecom@LC08L5714>
2017-05-23 10:43:52 -0400

    2305 the best

---
+++ revision.php
@@ -0,0 +1,17 @@
+<?php 
+$PATH='lib/';
+include $PATH.'Conexion.php';
+include $PATH.'Usuario.php';
+$oUsr=new Usuario();
+$oUsr->nombre=$_POST['nombre'];
+$oUsr->clave=$_POST['clave'];
+if($oUsr->VerificaUsuarioClave())
+    $_SESSION['USR']=$oUsr;
+    
+header('Location:http://localhost:8081/proy2305/');
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
